84b76c40148931b5e4b6b53fd66635380271735e
Wonderlyve <c.batuemi@gmail.com>
2025-07-23 12:47:22 +0100

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -52,11 +52,6 @@ export default {
 					DEFAULT: 'hsl(var(--card))',
 					foreground: 'hsl(var(--card-foreground))'
 				},
-				elegant: {
-					text: 'hsl(var(--elegant-text))',
-					subtle: 'hsl(var(--subtle-gray))',
-					pure: 'hsl(var(--pure-white))'
-				},
 				sidebar: {
 					DEFAULT: 'hsl(var(--sidebar-background))',
 					foreground: 'hsl(var(--sidebar-foreground))',
